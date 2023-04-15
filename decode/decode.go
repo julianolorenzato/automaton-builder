f0fd845a6a14ce626fcfc82a3c83fa1949b1ab40
Juliano Lorenzato <julianolorenzato@gmail.com>
2023-04-15 17:54:15 -0300

    feat: validations

--- decode/decode.go
+++ decode/decode.go
@@ -53,12 +53,14 @@ func Decode() (*AutomatonDescription, error) {
 func validateChain(ad *AutomatonDescription) error {
 	errs := errors.Join(
 		validateInitialStates(ad),
+		validateFinalStates(ad),
+		validateStatesName(ad),
 	)
 
 	return errs
 }
 
-// Verify if the automaton have exactly one initial state
+// Verify if the automaton description have exactly one initial state
 func validateInitialStates(ad *AutomatonDescription) error {
 	quantity := 0
 
@@ -69,9 +71,43 @@ func validateInitialStates(ad *AutomatonDescription) error {
 	}
 
 	if quantity == 0 {
-		return errors.New("input doesnt have an initial state")
+		return errors.New("automaton description doesnt have an initial state")
 	} else if quantity > 1 {
-		return errors.New("input have more than one initial state")
+		return errors.New("automaton description have more than one initial state")
+	}
+
+	return nil
+}
+
+// Verify if the automaton description have at least one final state
+func validateFinalStates(ad *AutomatonDescription) error {
+	quantity := 0
+
+	for _, v := range ad.States {
+		if v.Final {
+			quantity++
+		}
+	}
+
+	if quantity == 0 {
+		return errors.New("automaton description doesnt have at least final states")
+	}
+
+	return nil
+}
+
+// Verify if the automaton does not have a duplicate state name
+func validateStatesName(ad *AutomatonDescription) error {
+	eachQuantity := map[string]int{}
+
+	for _, v := range ad.States {
+		eachQuantity[v.Name]++
+	}
+
+	for k, v := range eachQuantity {
+		if v > 1 {
+			return errors.New(`automaton description have more than one state name "` + k + `"`)
+		}
 	}
 
 	return nil
